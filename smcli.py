ff70b5b68bd252386d466d2e7a6c4c160eceb3c4
Greg Baker <gregb@ifost.org.au>
2010-11-22 04:24:00 +0000

    Return error codes, cope with MS-Windows-style command lines.

--- smcli.py
+++ smcli.py
@@ -21,6 +21,7 @@ import logging
 from optparse import OptionParser
 from optparse import OptionGroup
 import sys
+import re
 
 # This program requires the python SUDS package. If the import in the
 # next line fails, then try
@@ -32,22 +33,6 @@ from suds.transport.http import HttpAuthenticated
 
 logging.basicConfig(level=logging.ERROR)
 
-######################################################################
-
-def camel2unix(x):
-    """A convenience function when you want to go from SomethingCamelCase to something-camel-case"""
-    answer = x[0].lower()
-    for i in range(1,len(x)-1):
-        answer = answer + x[i].lower()
-        if x[i].islower() and x[i+1].isupper():
-            answer = answer + '-'
-        if (i+2<len(x)) and x[i].isupper() and x[i+1].isupper() and x[i+2].islower():
-            answer = answer + '-'
-    answer = answer + x[-1].lower()
-    return answer
-
-
-######################################################################
 
 
 wsdl_paths = { INCIDENT : "IncidentManagement.wsdl",
@@ -55,6 +40,9 @@ wsdl_paths = { INCIDENT : "IncidentManagement.wsdl",
                CONFIGURATION: "ConfigurationManagement.wsdl",
                CONTACTS: "ConfigurationManagement.wsdl",
                }
+
+class UpdateException(Exception):
+    pass
 
 class smwsdl:
     """Reads config files from /etc/smwsdl.ini, ~/.smswsdl.ini,
@@ -106,17 +94,15 @@ to."""
                     helptext = "Set the "+field+" field (no default)"
             else:
                 helptext = "The "+field+" field."
-            unixified = camel2unix(field)
             if modelthing.keys.__dict__.has_key(field) and include_keys:
                 group = key_group
             elif modelthing.instance.__dict__.has_key(field) and include_instance:
                 group = instance_group
             else:
                 group = parser
-            group.add_option("--"+unixified,dest=field,type='string',
-                              action="store",help=helptext)
-
-
+            group.add_option("--"+camel2unix(field),"--"+field,
+                             dest=field,type='string',
+                             action="store",help=helptext)
         # To-do. It should also iterate of modelthing.keys, and put them
         # into a separate option group. We should have an argument to say
         # whether we are creating or doing some kind of update.
@@ -149,19 +135,19 @@ to."""
         self.__default_section = self.__sm_module + " defaults"
 
     def __read_config(self):
-        if sys.platform[:3] == 'win':
+        if os.name == 'nt':
             config_file_locations = [ 'smwsdl.ini' ]
             # Perhaps I should look up the registry instead?
             # Look in the install location?
-        elif sys.platform == 'vms':
-            sys.exit("I don't have a VMS box to test. What config files should this read?")
-        else:
+        elif os.name == 'posix':
             config_file_locations = ['/etc/smwsdl.ini',
                                      os.path.expanduser('~/.smwsdl.ini'),
                                      '.smwsdl.ini'
                                      ]
-        if os.environ.has_key('SMWSDL_CONF'):
-            config_file_locations.append(os.environ['SMWSDL_CONF'])
+            if os.environ.has_key('SMWSDL_CONF'):
+                config_file_locations.append(os.environ['SMWSDL_CONF'])
+        else:
+            sys.exit("Don't know where to look for config files on "+os.name)
 
         self.__config = ConfigParser.ConfigParser()
         files_read = self.__config.read(config_file_locations)
@@ -207,32 +193,51 @@ to."""
                               password=self.__service_manager_password)
         self.__client = Client(url,transport=t)
 
+
+######################################################################
+
+def camel2unix(x):
+    """A convenience function when you want to go from SomethingCamelCase to something-camel-case"""
+    answer = x[0].lower()
+    for i in range(1,len(x)-1):
+        answer = answer + x[i].lower()
+        if x[i].islower() and x[i+1].isupper():
+            answer = answer + '-'
+        if (i+2<len(x)) and x[i].isupper() and x[i+1].isupper() and x[i+2].islower():
+            answer = answer + '-'
+    answer = answer + x[-1].lower()
+    return answer
+
+
+######################################################################
                       
 
 
-def typical_update_program(sm_module,search_arg_type,invocation,uses_values=True,print_return=False):
+def typical_update_program(sm_module,cmdline,search_arg_type,invocation,uses_values=True,print_return=False):
     web_service = smwsdl(sm_module)
     parser = OptionParser(usage="usage: %prog --"+sm_module+"-id=...",version=version)
     web_service.add_to_command_line_parser(parser,search_arg_type,include_instance=uses_values)
-    (options,args) = parser.parse_args()
+    (options,args) = parser.parse_args(cmdline)
     new_incident = web_service.create_soap_object(search_arg_type,options.__dict__)
     answer = web_service.invoke(invocation,new_incident)
     ret = []
+    if not(answer.__dict__.has_key('messages')):
+        # Something really bad happened
+        raise UpdateException
     for m in answer.messages.message:
         ret.append(m.value)
-    sys.stderr.write(`answer`)
     if print_return:
         sys.stderr.write(string.join(ret,'\n')+'\n')
+    return ret
 
-
-def typical_create_program(sm_module,creation_arg_type,invocation,return_part=None,print_return=False):
+def typical_create_program(sm_module,cmdline,creation_arg_type,invocation,return_part=None,print_return=False):
     web_service = smwsdl(sm_module)
     parser = OptionParser(usage="usage: %prog --field-name=Value ...",
                           version=version)
     web_service.add_to_command_line_parser(parser,creation_arg_type,
                                            include_keys=False,
                                            provide_defaults=True)
-    (options,args) = parser.parse_args()
+    (options,args) = parser.parse_args(cmdline)
     new_incident = web_service.create_soap_object(creation_arg_type,options.__dict__)
     answer = web_service.invoke(invocation,new_incident)
     for m in answer.messages.message:
@@ -244,16 +249,16 @@ def typical_create_program(sm_module,creation_arg_type,invocation,return_part=No
         ret = answer.model.instance.__dict__[return_part].value
     if print_return:
         print ret
+    return ret
 
-
-def typical_search_program(sm_module,search_arg_type,invocation,return_part=None,print_return=False):
+def typical_search_program(sm_module,cmdline,search_arg_type,invocation,return_part=None,print_return=False):
     web_service = smwsdl(sm_module)
     parser = OptionParser(usage="usage: %prog --field=... --other-field=...",
                           version=version)
     web_service.add_to_command_line_parser(parser,search_arg_type,
                                            include_keys=False,
                                            provide_defaults=False)
-    (options,args) = parser.parse_args()
+    (options,args) = parser.parse_args(cmdline)
     new_incident = web_service.create_soap_object(search_arg_type,options.__dict__)
     answer = web_service.invoke(invocation,new_incident)
 
@@ -266,8 +271,9 @@ def typical_search_program(sm_module,search_arg_type,invocation,return_part=None
                 answers.append(k.__dict__[return_part].value)
     if print_return:
         print string.join(answers,'\n')
+    return answers
 
-def typical_retrieve_program(sm_module,search_arg_type,invocation,print_return=False):
+def typical_retrieve_program(sm_module,cmdline,search_arg_type,invocation,print_return=False):
     web_service = smwsdl(sm_module)
     parser = OptionParser(usage="usage: %prog --field=... --other-field=...",
                           version=version)
@@ -275,35 +281,47 @@ def typical_retrieve_program(sm_module,search_arg_type,invocation,print_return=F
                                            include_keys=True,
                                            include_instance=False,
                                            provide_defaults=False)
-    (options,args) = parser.parse_args()
+    (options,args) = parser.parse_args(cmdline)
     new_incident = web_service.create_soap_object(search_arg_type,options.__dict__)
     answer = web_service.invoke(invocation,new_incident)
     
     fields = answer.model.instance.__dict__.keys()
     fields.sort()
+
+    ret = {}
     for k in fields:
         if k[0]=="_": continue
-        if print_return: 
-            print k+": ",
         v = answer.model.instance.__dict__[k]
         if v._type == "Array":
-            first=True
+            ret[k] = []
             for elem in v.__dict__.keys():
                 if elem[0] == '_': continue
                 for subelem in v.__dict__[elem]:
-                    #if subelem[0]=="_": continue
-                    if print_return:
-                        if first: first=False
-                        else: print (" "*len(k+": ")),
-                        print subelem.value
+                    if subelem.__dict__.has_key('value'):
+                        ret[k].append(subelem.value)
         else:
-            if print_return:
-                print v.value
+            ret[k] = v.value
 
-def typical_list_methods_program(sm_module,print_return=False):
+    if print_return:
+        fields = ret.keys()
+        fields.sort()
+        for k in fields:
+            if type(ret[k])==type([]):
+                first_time=True
+                print k+": ",
+                for line in ret[k]:
+                    if first_time: first_time=False
+                    else: print (" "*(len(k+": "))),
+                    print line
+            else:
+                print k+": "+ret[k]
+    return ret
+
+def typical_list_methods_program(sm_module,cmdline,print_return=False):
     web_service = smwsdl(sm_module)
     if print_return:
         web_service.print_available_methods()
+    return None
 
 # To-do:
 # 1. Merge sm-*.py into here.
@@ -314,6 +332,8 @@ def typical_list_methods_program(sm_module,print_return=False):
 # 6. Usage argument should show what we are doing
 # 7. Fix up help so that it shows aliases as well
 # 8. Typical retrieve program should return a dictionary, I think.
+# 9. --foo-bar (unix) and /FooBar (windows) are equivalent. That
+#    will make it trickier
 
 actions = {
     'create' : { INCIDENT: { 'creation_arg_type': 'IncidentModelType',
@@ -321,14 +341,24 @@ actions = {
                              'return_part': 'IncidentID'  },
                  SERVICE_DESK: { 'creation_arg_type': 'InteractionModelType',
                                  'invocation': 'CreateInteraction',
-                                 'return_part': 'CallID'   }
+                                 'return_part': 'CallID'   },
+                 CONTACTS: {'creation_arg_type': 'ContactModelType',
+                            'invocation': 'CreateContact',
+                            'return_part': 'ContactName' },
                  },
     'close' :  { INCIDENT: { 'search_arg_type': 'IncidentModelType',
                              'invocation': 'CloseIncident',
-                             'uses_values':True }
+                             'uses_values':True },
+                 SERVICE_DESK: {'search_arg_type' : 'InteractionModelType',
+                                'invocation' : 'CloseInteraction',
+                                'uses_values':True }          
                  },
     'update' : { INCIDENT:  {'search_arg_type': 'IncidentModelType',
-                             'invocation': 'UpdateIncident' }
+                             'invocation': 'UpdateIncident' },
+                 SERVICE_DESK: {'search_arg_type':'InteractionModelType',
+                                'invocation': 'UpdateInteraction' },
+                 CONTACTS: {'search_arg_type': 'ContactModelType',
+                            'invocation' : 'UpdateContact' }
                  },
     'reopen' : { INCIDENT: { 'search_arg_type': 'IncidentModelType',
                              'invocation': 'ReopenIncident',
@@ -386,6 +416,7 @@ table_aliases = { 'incident' : INCIDENT,
                   'servicedesk' : SERVICE_DESK,
                   'service-desk' : SERVICE_DESK,
                   'call' : SERVICE_DESK,
+                  'calls' : SERVICE_DESK,
                   'contact' : CONTACTS,
                   'contacts' : CONTACTS,
                   'configuration' : CONFIGURATION
@@ -399,7 +430,24 @@ if __name__ == '__main__':
     if (table_aliases.has_key(table)):
         table = table_aliases[table]
 
-    sys.argv = sys.argv[2:]
+    sys.argv[0] = sys.argv[0]+" "+action+" " + table
+    cmdline = sys.argv[3:]
+    if os.name == 'nt':
+        # Allow users to use /Foo:bar instead of --Foo=bar
+        slashcolonsomething = re.compile('^/([A-Za-z0-9_]+):(.+)$')
+        slashcolononly = re.compile('^/([A-Za-z0-9_]+):')
+        justslash = re.compile('^/')
+        for i in range(len(cmdline)):
+            if slashcolonsomething.match(cmdline[i]):
+                cmdline[i] = slashcolonsomething.sub('--\\1=\\2',cmdline[i])
+                continue
+            if slashcolononly.match(cmdline[i]):
+                cmdline[i] = slashcolononly.sub('--\\1',cmdline[i])
+                continue
+            if justslash.match(cmdline[i]):
+                cmdline[i] = justslash.sub('--',cmdline[i])
+                continue
+
     if not(actions.has_key(action)):
         sys.exit("Unknown action. Actions are: "+string.join(actions.keys()," "))
     if not(actions[action].has_key(table)):
@@ -407,6 +455,7 @@ if __name__ == '__main__':
                  string.join(actions[action].keys()," "))
     kwargs = actions[action][table]
     function = function_calls[action]
-    function(table,print_return=True,**kwargs)
-    
-
+    try:
+        function(table,print_return=True,cmdline=cmdline,**kwargs)
+    except UpdateException:
+        sys.exit("Update failure")
