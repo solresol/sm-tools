3df60bc41794501286078b2270476a065db17158
Greg Baker <gregb@ifost.org.au>
2010-11-22 07:05:45 +0000

    Don't need these files any more.

--- sm-list-configuration-methods.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# This program was developed by Greg Baker <gregb@ifost.org.au> (c) 2010
-
-import smcli
-
-web_service = smwsdl.smwsdl(smwsdl.CONFIGURATION)
-
-web_service.print_available_methods()
--- sm-list-incident-methods.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# This program was developed by Greg Baker <gregb@ifost.org.au> (c) 2010
-
-import smcli
-
-web_service = smcli.smwsdl(smcli.INCIDENT)
-
-web_service.print_available_methods()
