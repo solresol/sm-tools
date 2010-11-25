e0c29a531710eb5a891f6d3e7c6c92316ed758d5
Greg Baker <gregb@ifost.org.au>
2010-11-25 03:05:55 +0000

    Now we can send emails out too. What do we need Connect-IT for?

---
+++ sm2email.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import smtplib
+import ConfigParser
+import email
+import smcli
+from email.MIMEText import MIMEText
+
+my_config = ConfigParser.ConfigParser()
+read_ok = my_config.read(['sm2email.conf'])
+if read_ok == []:
+    sys.exit("Couldn't read sm2email.conf")
+
+smtp_server = my_config.get('smtp','server')
+if my_config.has_option('smtp','port'):
+    smtp_port = my_config.getint('smtp','port')
+else:
+    smtp_port = 25
+# Perhaps should add option for local_hostname and timeout?
+
+from_address = my_config.get('smtp','from_address')
+
+mail_connection = smtplib.SMTP(smtp_server,smtp_port)
+
+outgoings = smcli.typical_search_program(smcli.EVENTOUT,
+                                         ['--evtype=email'],
+                                         'search')
+
+for evsys in outgoings:
+    e = smcli.typical_retrieve_program(smcli.EVENTOUT,
+                                       ['--evsysseq',evsys],
+                                       'retrieve'
+                                       )
+    sep = e['Evsepchar']
+    data = e['Evfields'].split(sep)
+    dest_email = data[0]
+    source = data[1]
+    contact = data[2]
+    subject = data[3]
+    body = data[4]
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['To'] = dest_email
+    msg['From'] = source  ;# not sure if that is sensible or not
+    #print msg.as_string()
+
+    #print "="*70
+    mail_connection.sendmail(from_address,[dest_email],msg.as_string())
+    print "Sent message to",dest_email,"with subject",subject
+    smcli.typical_delete_program(smcli.EVENTOUT,
+                                 ['--evsysseq',evsys],
+                                 'delete')
+
+mail_connection.quit()
