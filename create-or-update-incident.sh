53e75872902e78b4bf5ca4ad495ffa4ca8f3e52d
Greg Baker <gregb@ifost.org.au>
2010-12-24 09:43:49 +0000

    Now it uses nnmid

--- create-or-update-incident.sh
+++ create-or-update-incident.sh
@@ -3,11 +3,15 @@
 # $Id$
 
 # This script's first argument should be a uuid (e.g. $uuid from an NNM
-# incident). This will be searched for in the description field.
+# incident).
 # 
 # The second argument should be whatever you want to the
 # description field to say (onto which the uuid will be appended);
 # if it is blank or "-" then description won't be updated.
+#
+# Real soon now, we'll ditch this second argument business, because
+# description is no longer particularly special.
+# We will need computer and interface though.
 #
 # Any remaining arguments will be passed to sm-update-incident.py or
 # sm-create-incident.py.
@@ -24,16 +28,16 @@ fi
 
 PATH=/var/opt/OV/shared/nnm/actions:$PATH
 
-INCIDENTS=$(smcli.py search incidents --description="*uuid=$UUID*")
+INCIDENTS=$(sm-search-incidents.py --nnmid="$UUID")
 
 if [ "$INCIDENTS" = "" ]
 then
-  smcli.py create incident --description="$DESCRIPTION  uuid=$UUID" "$@"
+  sm-create-incident.py --description="$DESCRIPTION" --nnmid="$UUID" "$@"
 else
     if [ "$DESCRIPTION" = "" ]
     then
-	smcli.py create incident --incident-id=$INCIDENTS "$@"
+	sm-update-incident.py --incident-id=$INCIDENTS "$@"
     else
-	smcli.py update incident --incident-id=$INCIDENTS --description="$DESCRIPTION  uuid=$UUID" "$@"
+	sm-update-incident.py --incident-id=$INCIDENTS --description="$DESCRIPTION  uuid=$UUID" "$@"
     fi
 fi
